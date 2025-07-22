434e015a3576470eb92572ed926d21aa4e647fb0
Sujoy Sen <144342152+Kingjoy7@users.noreply.github.com>
2025-07-22 22:58:18 +0530

    Update connectdb.js

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(`mongodb://localhost:27017/PatreonHive`, {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
-        console.log(`MongoDB Connected: {conn.connection.host}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(error.message);
+        console.error("MongoDB connection failed:", error.message);
         process.exit(1);
     }
-}
-export default connectDB
+};
+
+export default connectDB;
